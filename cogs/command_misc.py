e688f6fc1a5b0910d49e295536223dce73fe4444
TickerOfTime <jreasley14@gmail.com>
2016-10-14 18:18:59 -0500

    final fix of typos, removed useless json files

--- cogs/command_misc.py
+++ cogs/command_misc.py
@@ -16,6 +16,34 @@ class Misc:
   def __init__(self, bot):
     self.bot = bot
 
+  @commands.command(name="retro", brief="make retro text")
+  async def make_retro(self, *, content : str):
+    texts = [t.strip() for t in content.split('|')]
+    if len(texts) != 3:
+      await self.bot.say("\N{CROSS MARK} please provide three strings seperated by `|`")
+      return
+
+    await self.bot.type()
+
+    data = dict(
+      bcg=choice([1, 2, 3, 4]),
+      txt=choice([1, 2, 3, 4]),
+      text1=texts[0],
+      text2=texts[1],
+      text3=texts[2]
+    )
+
+    with aiohttp.ClientSession() as session:
+      async with session.post("http://photofunia.com/effects/retro-wave", data=data) as response:
+        if response.status == 200:
+          soup = b_s(await response.text(), "lxml")
+          download_url = soup.find("div", class_="downloads-container").ul.li.a["href"]
+          async with session.get(download_url) as image_response:
+            if image_response.status == 200:
+              image_data = await image_response.read()
+              with BytesIO(image_data) as temp_image:
+                await self.bot.upload(temp_image, filename="retro.jpg")
+
   @commands.command(name="xkcd", brief="get an xkcd comic")
   async def get_xkcd(self, id : str = ''):
     await self.bot.type()
